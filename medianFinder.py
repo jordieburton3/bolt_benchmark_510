31ce1a93a5137b86c9b5aec219224bbebafea0b4
Jordan Burton <jordieburton3@gmail.com>
2018-04-17 16:43:43 -0400

    added median finder

---
+++ medianFinder.py
@@ -0,0 +1,29 @@
+
+
+def getMedian():
+    allTimes = []
+    numLines = 0
+    for i in range(0, 10):
+        f = open('raw' + str(i) + '.txt', 'r')
+        for line in f:
+            time = line.split()[-1]
+            if 'ms' in time:
+                try:
+                    allTimes.append(float(time.replace('ms', '')))
+                except:
+                    continue
+            elif 's' in time:
+                try:
+                    allTimes.append(float(time.replace('s', '')) * 1000.0)
+                except:
+                    continue
+            numLines += 1
+        f.close()
+    middleIndex = len(allTimes) / 2
+    median = allTimes[middleIndex]
+    print "Number of lines: ", numLines
+    print median
+    return median
+
+if __name__ == "__main__":
+    getMedian()
